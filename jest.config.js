88184bbd697f3a8dfe351f21d567fbfb135877c9
minotaurrr <kwangmin.kim@hotmail.com>
2021-09-06 00:19:29 +1000

    add coverage & test file match

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
-const { compilerOptions } = require("./tsconfig");
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  setupFiles: ["<rootDir>/.jest/setup.js"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFiles: ['<rootDir>/.jest/setup.js'],
   clearMocks: true,
-  roots: ["<rootDir>"],
+  roots: ['<rootDir>'],
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  testMatch: ['**/?(*.)+(spec|test).ts'],
 };
