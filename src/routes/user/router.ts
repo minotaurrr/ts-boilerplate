04816a11e58532762820b4c4db29c24eaed1c533
minotaurrr <kwangmin.kim@hotmail.com>
2022-05-14 21:45:14 +1000

    remove unnecessary next function param

--- src/routes/user/router.ts
+++ src/routes/user/router.ts
@@ -4,7 +4,7 @@ import { UserServiceType } from 'services/user/types';
 export const UserRouter = ({ service }: { service: UserServiceType }) => {
   const router = express.Router();
 
-  router.get('/', (req, res, next) => {
+  router.get('/', (req, res) => {
     const { username } = req.query;
 
     const user = service.findUserByUsername(String(username));
